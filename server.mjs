6ee0f1a59811856bf851b438599b8d9fb926d6ed
Silvina Figueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2024-08-21 17:19:49 -0700

    using cors in vercel.json only

--- server.mjs
+++ server.mjs
@@ -16,29 +16,29 @@ const frontendURL = 'https://journai-frontend.vercel.app'
 // // Middleware to apply CORS custom function to all routes
 // app.use((req, res, next) => allowCors(next, frontendURL)(req, res));
 
-// Apply CORS Middleware
-app.use(cors({
-    origin: frontendURL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'],
-    credentials: true
-}))
+// // Apply CORS Middleware
+// app.use(cors({
+//     origin: frontendURL,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'],
+//     credentials: true
+// }))
 
-// Handle preflight `OPTIONS` requests globally
-app.options('*', (req, res) => {
-    console.log('Received OPTIONS request for:', req.path);  // Log the request path
-    console.log('Origin:', req.headers.origin);              // Log the request origin
-    console.log('Requested Method:', req.headers['access-control-request-method']) // Log the requested method
-    console.log('Requested Headers:', req.headers['access-control-request-headers']) // Log the requested headers
+// // Handle preflight `OPTIONS` requests globally
+// app.options('*', (req, res) => {
+//     console.log('Received OPTIONS request for:', req.path);  // Log the request path
+//     console.log('Origin:', req.headers.origin);              // Log the request origin
+//     console.log('Requested Method:', req.headers['access-control-request-method']) // Log the requested method
+//     console.log('Requested Headers:', req.headers['access-control-request-headers']) // Log the requested headers
 
-    res.setHeader('Access-Control-Allow-Origin', frontendURL)
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-auth-token')
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     res.setHeader('Access-Control-Allow-Origin', frontendURL)
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-auth-token')
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
     
-    console.log('Responding with 200 OK and CORS headers') // Log the response action
-    res.sendStatus(200); // Respond with HTTP 200 status
-})
+//     console.log('Responding with 200 OK and CORS headers') // Log the response action
+//     res.sendStatus(200); // Respond with HTTP 200 status
+// })
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS#preflighted_requests
 
 
