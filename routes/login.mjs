4e7d464bc6090c0196683bd3265c60c6cecdfca1
Silvina Figueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2024-08-21 17:29:31 -0700

    adding more debugging for cors

--- routes/login.mjs
+++ routes/login.mjs
@@ -7,16 +7,15 @@ import login from '../controllers/loginControllers.mjs'
 // Express Router
 const router = express.Router()
 
+router.options('/', (req, res) => {
+    console.log('OPTIONS request received on /login')
+    res.status(200).send('OK')})
+
 router.post('/', [
     // Validation array: check(parameter, error message).validation function()
     check('email', "Include a valid email").isEmail(),
     check('password', "Enter a password with 8 or more characters").isLength({min: 8})
     ], login.userLogin)
-
-
-router.options('/', (req, res) => {
-    console.log('OPTIONS request received on /login')
-    res.status(200).send('OK')})
 
 
 export default router
